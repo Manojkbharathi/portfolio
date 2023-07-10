15eb1c203e2c6f564fe039b29a0e4283e57a5d54
Manojkbharathi <manojbharathi00@gmail.com>
2023-07-10 10:45:22 +0530

    modified skills

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,6 +5,12 @@ import Profile from '../assets/20230626_092109.png';
 const Header = () => {
   return (
     <div>
+      <h1>
+        <span>M</span>
+        <span>A</span>
+        <span>N</span>
+        <span>O</span>
+      </h1>
       <h2 className='head'>{'<Hello world/>'}</h2>
       <div className='home' id='home'>
         <img src={Profile} alt='' className='profile' />
